2efa516811a3fe564d77a7166dd90f49332b86f2
SrVicenteHD <vi_higino@hotmail.com.br>
2021-06-22 16:49:15 -0300

    Backend: Improving how the images are saved

--- server/routes/SaveImageRoute.js
+++ server/routes/SaveImageRoute.js
@@ -9,19 +9,15 @@ export async function SaveImageRoute(req, res) {
       throw new Error("No image");
     }
     const { ext, mime } = await FileType.fromBuffer(req.body);
-    const text = await req.body;
-    const name = crypto.createHash("SHA1").update(text).digest('hex');
-    const firtPart = "./images/" + name.substr(0, 2);
-    const secondPart = firtPart + "/" + name.substr(2, 2) + "/";
-    const imagePath = path.join(secondPart, name + "." + ext);
-    if (!fs.existsSync("./images"))
-      fs.mkdirSync("./images");
-    if (!fs.existsSync(firtPart))
-      fs.mkdirSync(firtPart);
-    if (!fs.existsSync(secondPart))
-      fs.mkdirSync(secondPart);
-    fs.writeFileSync(imagePath, text);
-    res.json({ url: "/" + imagePath, type: mime });
+    const imageBytes = await req.body;
+    const name = crypto.createHash("SHA1").update(imageBytes).digest('hex');
+    const fullPath = path.join("./images/", name.substr(0, 2), name.substr(2, 2));
+    const imagePath = path.join(fullPath, `${name}.${ext}`);
+    if (!fs.existsSync(imagePath)) {
+      if (!fs.existsSync(fullPath)) fs.mkdirSync(fullPath, { recursive: true });
+      fs.writeFileSync(imagePath, imageBytes);
+    }
+    res.json({ url: `/${imagePath}`, type: mime });
   } catch (error) {
     res.status(400).json({ message: "Image coud not be saved!" });
   }
