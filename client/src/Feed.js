83e20c46486bb7a2da5812dff3f5bff8cb67b509
SrVicenteHD <vi_higino@hotmail.com.br>
2021-06-23 01:22:52 -0300

    Frontend: Adding a proper carousel to posts

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { getTimePassed } from "./utils";
 import { url, StateContext } from "./App"
 import axios from "axios";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 export function Feed({ posts, inview }) {
   const [state] = useContext(StateContext);
   const feed = posts.map((post) => {
@@ -24,7 +25,7 @@ function Post({ post, state, inview }) {
       <p className="post-sideTime">{getTimePassed(post.createdAt)}</p>
     </div>
     {post.text && <p className="post-mainText">{post.text}</p>}
-    {post?.images?.length > 0 && <ImageContainer images={post.images} />}
+    {post?.images?.length > 0 && <Images images={post.images} />}
     <div className="post-footer">
       <p className="post-like">{likes.likes}</p>
       <button className="post-button-like" onClick={async () => {
@@ -50,68 +51,27 @@ function profileLink(post, state) {
   return post.profile.username === state.profile.username ? "/myprofile" : `/profile/${post.profile.username}`;
 }
 
-function ImageContainer({ images }) {
-  const imgs = useRef();
-  return <div style={{ position: "relative" }}>
-    <div ref={imgs} className="image-container">
-      <Images images={images} />
-    </div>
-  </div>
-
-}
-function ImagesButtons({ imgs }) {
-  return <>
-    <div className="left-img-button img-button" onClick={(e) => {
-      const activeElement = imgs.current.getElementsByClassName("img-active")[0];
-      const previousElement = activeElement.previousElementSibling;
-      if (previousElement) {
-        activeElement.classList.remove("img-active");
-        previousElement.classList.add("img-active");
-      }
-    }}>
-      <svg width="50" height="50" className="arrow-left-white" viewBox="0 0 10 10" fill="none">
-        <path d="M5 7L1 3H9L5 7Z" fill="currentColor" />
-      </svg>
-
-    </div>
-    <div className="right-img-button img-button" onClick={(e) => {
-      const activeElement = imgs.current.getElementsByClassName("img-active")[0];
-      const nextElement = activeElement.nextElementSibling;
-      if (nextElement) {
-        activeElement.classList.remove("img-active");
-        nextElement.classList.add("img-active");
-      }
-    }}><svg width="50" height="50" className="arrow-right-white" viewBox="0 0 10 10" fill="none">
-        <path d="M5 7L1 3H9L5 7Z" fill="currentColor" />
-      </svg></div>
-  </>
-}
 function Images({ images }) {
-  return images.map((image, index) => {
-    return <Image image={image}></Image>
-  })
+  return <Carousel onClickItem={(i, item) => console.log(item)}
+    autoPlay={false} infiniteLoop={true} showStatus={false} showThumbs={false} showIndicators={images.length > 1 ? true : false}>
+    {
+      images.map((image) => {
+        return <Image key={image.url} image={image}></Image>
+      })
+    }
+  </Carousel >
 
 }
 
 function Image({ image }) {
   if (!image) return;
   if (image.type.startsWith("image")) return <img loading="lazy"
-    /*onClick={(e) => fullImg(e)}*/
-    className="post-img"
-    src={url + image.url} key={image.url} />
+    src={url + image.url} />
   if (image.type.startsWith("video")) return <video loading="lazy"
-    // onTouchEnd={(e) => fullImg(e)}
-    // onClick={(e) => fullImg(e)}
-    controls loop autoPlay muted
-    className="post-img"
-    src={url + image.url} key={image.url} />
+    controls muted
+    src={url + image.url} />
 }
 
-function fullImg(e) {
-  e.preventDefault();
-  const img = e.target;
-  const root = document.getElementsByTagName("body")[0]
-  root.style.overflow = root.style.overflow === "hidden" ? "auto" : "hidden";
-  img.classList.contains("full-img") ? img.classList.remove("full-img") : img.classList.add("full-img");
-
+function FullScrenImage({ image }) {
+  return <img className="fullScreen" src={image.url} />
 }
